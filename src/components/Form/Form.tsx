57503b90bbde677ae1867f3ee0265a9493cf85e1
Sooseki <manon.bonnet.30@gmail.com>
2022-09-05 13:04:13 +0200

    Form template

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import './Form.css';
 
-interface props {
-  form: {
-    label: string,
-    type: string,
-    handleSubmit: React.FormEventHandler<HTMLFormElement>
-  }
+type Input = {
+  name: string; 
+  type: string; 
+  placeholder: string; 
 }
 
-function Form({ form }: props ) {
+interface props {
+  form: {
+    inputs: Input[],
+    handleSubmit: React.FormEventHandler<HTMLFormElement>
+  },
+  setData: React.Dispatch<React.SetStateAction<{}>>
+}
+
+const Form = ({form, setData}: props) => {
+
+  const updateForm = (e: React.ChangeEvent<HTMLInputElement>, name: any) => {
+    setData((data: any) => ({
+        ...data,
+        [name]: e.target.value,
+    }))
+  }
+
   return (
     <div className="Form">
       <form onSubmit={form.handleSubmit}>
-        <input type={form.type} placeholder={form.label} />
+        {form.inputs.map((key, index, input) => {
+          return (
+            <div>
+              <input type={form.inputs[index].type} onChange={(e) => updateForm(e, form.inputs[index].name)} placeholder=' ' />
+              <label>{form.inputs[index].placeholder}</label>
+            </div>
+          );
+        })}
+        <input type="submit" className="submit" value="Valider" />
       </form>
     </div>
-  );
+  )
 }
+
 
 export default Form;
--- src/templates/Login/Login.tsx
+++ src/templates/Login/Login.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../../Components/Form/Form';
 import './Login.css';
 
-function Login() {
+function Login() {  
+  
+  const [data, setData] = useState({})
+
+  const sendUsername = (e: any) => {
+    e.preventDefault()
+    console.log("good good")
+  }
+
+  const form = {
+    inputs: [
+      {      
+        'name': 'email',
+        'type': 'email', 
+        'placeholder': 'Email',
+      },
+      {      
+        'name': 'password',
+        'type': 'password', 
+        'placeholder': 'Password',
+      }
+    ],
+    handleSubmit: sendUsername
+  }
+
   return (
     <div className="Login">
+      <Form form={form} setData={setData} />
     </div>
   );
 }
