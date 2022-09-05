ab779face7afcb87b663b0a70b4a16349f892b7f
Sooseki <manon.bonnet.30@gmail.com>
2022-09-05 12:26:53 +0200

    form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Form.css';
 
-function Form() {
+interface props {
+  form: {
+    label: string,
+    type: string,
+    handleSubmit: React.FormEventHandler<HTMLFormElement>
+  }
+}
+
+function Form({ form }: props ) {
   return (
     <div className="Form">
+      <form onSubmit={form.handleSubmit}>
+        <input type={form.type} placeholder={form.label} />
+      </form>
     </div>
   );
 }
