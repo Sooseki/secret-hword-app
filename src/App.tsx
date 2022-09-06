815b22873a66e1d45d601b144c1d733680689877
aparent <alexp90300@gmail.com>
2022-09-06 15:46:39 +0200

    feat: socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { emit } from 'process';
 import React from 'react';
 import './App.css';
 import Router from "./routes/Routes"
-
-
+import { io } from 'socket.io-client';
+const socket = io("http://localhost:5555");
 function App() {
+
+  const sendPseudo = () => {
+  //  socket.emit()
+  }
   return (
     <div className="App">
       <Router></Router>
