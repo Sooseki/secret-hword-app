0fcca779d57bfaeb0e8f140e1f4d1adb85387059
david <david.dehe.pro@gmail.com>
2022-09-08 00:03:09 +0200

    Login page style

--- src/templates/Login/Login.tsx
+++ src/templates/Login/Login.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import Form from '../../components/Form/Form';
-import './Login.scss';
+import React, { useState } from "react"
+import Form from "../../components/Form/Form"
+import "./Login.scss"
 
-function Login() {  
-  
+import { SvgCards } from "../../assets/SvgCards"
+import Title from "../../assets/imgs/title.png"
+
+export const Login = () => {
   const [data, setData] = useState({})
 
   const sendUsername = (e: any) => {
@@ -13,20 +15,25 @@ function Login() {
 
   const form = {
     inputs: [
-      {      
-        'name': 'pseudo',
-        'type': 'text',
-        'placeholder': 'Enter your 39 - 45 name',
+      {
+        name: "pseudo",
+        type: "text",
+        placeholder: "Enter your 39 - 45 name",
       },
     ],
-    handleSubmit: sendUsername
+    handleSubmit: sendUsername,
   }
 
   return (
-    <div className="Login">
-      <Form form={form} setData={setData} />
+    <div className="loginPage">
+      <div className="bannerContainer">
+        <div>
+          <SvgCards size={["30%", "30%"]} />
+        </div>
+        <img src={Title} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
