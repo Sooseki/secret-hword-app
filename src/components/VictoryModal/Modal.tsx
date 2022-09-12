3593901bbc61f2461b0d8f50adf959aeef9ea999
Robin <robinpinondev@gmail.com>
2022-09-12 14:39:20 +0200

    add btn modal

--- src/components/VictoryModal/Modal.tsx
+++ src/components/VictoryModal/Modal.tsx
@@ -1,23 +1,35 @@
 import ReactDOM from "react-dom";
+import { useNavigate } from "react-router-dom";
 
 interface props {
-    gameWon: string
+  gameWon: string;
 }
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 
-const Modal:any = ({gameWon}: props) => {
-    return (
-        ReactDOM.createPortal(
-            <>
-                <div className="modal">
-                    <div className="modal-content">
-                        {gameWon} won
-                    </div>
-                </div>
-            </>, container
-        )
-    )
-}
+const Modal: any = ({ gameWon }: props) => {
+  const navigate = useNavigate();
+  const replayGame = () => {
+    localStorage.clear();
+    navigate("/", { replace: true });
+  };
+  return ReactDOM.createPortal(
+    <>
+      <div className="modal">
+        <div className="modal-content">
+          {gameWon} won
+          {gameWon && (
+            <div className="replayGame">
+              <button onClick={replayGame} className="replayGame">
+                Nouvelle partie
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>,
+    container
+  );
+};
 
 export default Modal;
